79eced2e8d50b85f033a93bbc554fa25e638bf93
LuisCutilak <luiscutilak@gmail.com>
2023-10-12 17:02:55 -0300

    putting methods

---
+++ todolist2/src/main/java/br/com/luiscutilak/todolist2/user/UserModel.java
@@ -0,0 +1,33 @@
+package br.com.luiscutilak.todolist2.user;
+
+public class UserModel {
+    
+    private String username;
+    private String name;
+    private String passwword;
+
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPasswword() {
+        return this.passwword;
+    }
+
+    public void setPasswword(String passwword) {
+        this.passwword = passwword;
+    }
+}
