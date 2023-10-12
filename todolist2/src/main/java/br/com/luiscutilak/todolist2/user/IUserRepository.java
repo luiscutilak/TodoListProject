3b40fdd2603ebc339b724121623abd7fed2f2ce9
LuisCutilak <luiscutilak@gmail.com>
2023-10-12 18:35:04 -0300

    conditional, user already exists

--- todolist2/src/main/java/br/com/luiscutilak/todolist2/user/IUserRepository.java
+++ todolist2/src/main/java/br/com/luiscutilak/todolist2/user/IUserRepository.java
@@ -3,8 +3,10 @@ package br.com.luiscutilak.todolist2.user;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 //representação dos métodos
 public interface IUserRepository extends JpaRepository<UserModel, UUID> {
-    
+    UserModel findByUsername(String username);
 }
--- todolist2/src/main/java/br/com/luiscutilak/todolist2/user/UserModel.java
+++ todolist2/src/main/java/br/com/luiscutilak/todolist2/user/UserModel.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -15,7 +16,8 @@ public class UserModel {
     @Id
     @GeneratedValue(generator = "UUID")
     private UUID id;
-
+    
+    @Column(unique = true)
     private String username;
     private String name;
     private String password;
