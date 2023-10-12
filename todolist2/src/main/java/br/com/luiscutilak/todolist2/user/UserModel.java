438ec8eef7f3aad5607d1038d03a7892938d2353
LuisCutilak <luiscutilak@gmail.com>
2023-10-12 17:47:47 -0300

    Create Entity

--- todolist2/src/main/java/br/com/luiscutilak/todolist2/user/UserModel.java
+++ todolist2/src/main/java/br/com/luiscutilak/todolist2/user/UserModel.java
@@ -1,11 +1,43 @@
 package br.com.luiscutilak.todolist2.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_users")
 public class UserModel {
-    
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
     private String username;
     private String name;
-    private String passwword;
+    private String password;
 
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public UUID getId() {
+        return this.id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 
     public String getUsername() {
         return this.username;
@@ -23,11 +55,11 @@ public class UserModel {
         this.name = name;
     }
 
-    public String getPasswword() {
-        return this.passwword;
+    public String getPassword() {
+        return this.password;
     }
 
-    public void setPasswword(String passwword) {
-        this.passwword = passwword;
+    public void setPassword(String passwword) {
+        this.password = password;
     }
 }
