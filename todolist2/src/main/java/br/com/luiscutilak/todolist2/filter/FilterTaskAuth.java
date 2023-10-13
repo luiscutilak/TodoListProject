de1d4deb86b0f93b999119ac081465fe4bd54ac4
LuisCutilak <luiscutilak@gmail.com>
2023-10-13 10:15:40 -0300

    Data Validations, ok

--- todolist2/src/main/java/br/com/luiscutilak/todolist2/filter/FilterTaskAuth.java
+++ todolist2/src/main/java/br/com/luiscutilak/todolist2/filter/FilterTaskAuth.java
@@ -24,34 +24,44 @@ public class FilterTaskAuth extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        // Pegar a autenticação (usuario e senha)
-        var authorization = request.getHeader("Authorization");
-        var authEncoded = authorization.substring("Basic".length()).trim();
+        var servletPath = request.getServletPath();
 
-        byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+        if (servletPath.equals("/tasks/")) {
+            // Pegar a autenticação (usuario e senha)
+            var authorization = request.getHeader("Authorization");
+            var authEncoded = authorization.substring("Basic".length()).trim();
 
-        var authString = new String(authDecode);
-        // Separando usuario e senha para ser validado.
-        String[] credentials = authString.split(":");
-        String username = credentials[0];
-        String password = credentials[1];
+            byte[] authDecode = Base64.getDecoder().decode(authEncoded);
 
-        var user = this.userRepository.findByUsername(username);
-        // Abaixo validação de usuario e senha
-        // Se o usuario nao tem autorização ou nao valido erro 401
-        if (user == null) {
-            response.sendError(401);
-            // Senao entra no else e valida a senha e Segue viagem.
-        } else {
-            var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
-            if (passwordVerify.verified) {
-                filterChain.doFilter(request, response);
-                //Se senha der errada ou invalida entra no senao e error 401
-            } else {
+            var authString = new String(authDecode);
+            // Separando usuario e senha para ser validado.
+            String[] credentials = authString.split(":");
+            String username = credentials[0];
+            String password = credentials[1];
+
+            var user = this.userRepository.findByUsername(username);
+            // Abaixo validação de usuario e senha
+            // Se o usuario nao tem autorização ou nao valido erro 401
+            if (user == null) {
                 response.sendError(401);
+                // Senao entra no else e valida a senha e Segue viagem.
+            } else {
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                if (passwordVerify.verified) {
+                    //Recuperar atributo Id do UserModel,controller força isso
+                    request.setAttribute("idUser", user.getId());
+                    filterChain.doFilter(request, response);
+                    // Se senha der errada ou invalida entra no senao e error 401
+                } else {
+                    response.sendError(401);
+                }
+
             }
 
+        } else {
+            filterChain.doFilter(request, response);
         }
+
     }
 
 }
--- todolist2/src/main/java/br/com/luiscutilak/todolist2/task/TaskController.java
+++ todolist2/src/main/java/br/com/luiscutilak/todolist2/task/TaskController.java
@@ -1,10 +1,17 @@
 package br.com.luiscutilak.todolist2.task;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -14,8 +21,23 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")    
-    public TaskModel create(@RequestBody TaskModel taskModel) {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        System.out.println("Chegou no controller");
+        var idUser = request.getAttribute("idUser");
+        //Vincular Id usuario a tarefa
+        taskModel.setIdUser((UUID)idUser);
+        var currentDate = LocalDateTime.now();
+        //Se data atual maior que Data startada pela tarefa >> ERROR
+        if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início / data término, deve ser maior do que a data atual.");
+        }
+        // Se a data de inicio da tarefa for maior que a data de témino ERROR tbm.
+        if(taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início deve ser menor do que a data de término.");
+        }
+
         var task = this.taskRepository.save(taskModel);
-        return task;
+        return ResponseEntity.status(HttpStatus.OK).body(task);
+
     }
 }
