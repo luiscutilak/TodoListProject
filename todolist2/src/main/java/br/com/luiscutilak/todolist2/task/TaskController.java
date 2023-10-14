9173d76844e10f2b6e5dc7874c600688337c7fa5
LuisCutilak <luiscutilak@gmail.com>
2023-10-13 21:23:07 -0300

    PUTTING PARCIAL EDIT METHOD, LOGICA, ok

--- todolist2/src/main/java/br/com/luiscutilak/todolist2/task/TaskController.java
+++ todolist2/src/main/java/br/com/luiscutilak/todolist2/task/TaskController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.luiscutilak.todolist2.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -54,9 +55,15 @@ public class TaskController {
     //Alterar/Update nas tarefas(tasks) pelo id do user
     @PutMapping("/{id}")
     public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
-        var idUser = request.getAttribute("idUser");
-        taskModel.setIdUser((UUID)idUser);
-        taskModel.setId(id);
-        return this.taskRepository.save(taskModel);
+
+
+       var task = this.taskRepository.findById(id).orElse(null);
+
+       Utils.copyNonNullProperties(taskModel, task);
+
+
+        return this.taskRepository.save(task);
     }
+
+
 }
